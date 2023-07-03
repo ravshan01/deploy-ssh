4baf90a74bf5a11040d6a7abf4f1886118addb2b
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2023-07-03 20:05:42 +0500

    v1.1.1 support custom port

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { NodeSSH } from 'node-ssh';
 
 export default async function deploy({
   hostIP,
+  port,
   userName,
   userPassword,
   privateKey,
@@ -17,6 +18,7 @@ export default async function deploy({
   try {
     await ssh.connect({
       host: hostIP,
+      port,
       username: userName,
       password: userPassword,
       privateKey,
